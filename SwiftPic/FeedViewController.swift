8c8b4b04eb112b56d96596dfcc942d149ae3ff93
harmony222 <scarleth@oregonstate.edu>
2021-03-18 13:51:05 -0700

    user can signup, login, view posts, add post, pull to refresh, and infinite scroll

---
+++ SwiftPic/FeedViewController.swift
@@ -0,0 +1,192 @@
+//
+//  FeedViewController.swift
+//  SwiftPic
+//
+//  Created by Harmony Scarlet on 3/18/21.
+//
+
+import UIKit
+import Parse
+import AlamofireImage
+
+class FeedViewController: UIViewController, UITableViewDelegate, UITableViewDataSource, UIScrollViewDelegate {
+
+    @IBOutlet weak var tableView: UITableView!
+    
+    var posts = [PFObject]()
+    var numberOfCells: Int!
+    
+    let myRefreshControl = UIRefreshControl()
+    var isMoreDataLoading = false
+    var loadingMoreView:InfiniteScrollActivityView?
+
+//    let cellBuffer: CGFloat = 2
+//    let cellHeight: CGFloat = 445
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        tableView.delegate = self
+        tableView.dataSource = self
+        
+        loadCells()
+        
+        myRefreshControl.addTarget(self, action: #selector(loadCells), for: .valueChanged)
+        tableView.refreshControl = myRefreshControl
+        
+        // Set up Infinite Scroll loading indicator
+        let frame = CGRect(x: 0, y: tableView.contentSize.height, width: tableView.bounds.size.width, height: InfiniteScrollActivityView.defaultHeight)
+        loadingMoreView = InfiniteScrollActivityView(frame: frame)
+        loadingMoreView!.isHidden = true
+        tableView.addSubview(loadingMoreView!)
+        
+        var insets = tableView.contentInset
+        insets.bottom += InfiniteScrollActivityView.defaultHeight
+        tableView.contentInset = insets
+    
+       
+        // Do any additional setup after loading the view.
+    }
+    
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+        self.loadCells()
+    }
+    
+    @objc func loadCells() {
+        numberOfCells = 5
+        let query = PFQuery(className:"Posts")
+        query.includeKey("author")
+        query.order(byDescending: "createdAt")
+        query.limit = numberOfCells
+        
+        query.findObjectsInBackground { (posts, error) in
+            if posts != nil {
+                self.posts = posts!
+                self.tableView.reloadData()
+                self.myRefreshControl.endRefreshing()
+            } else {
+                print("unable to get posts")
+            }
+        }
+        print("loading cells")
+        print(posts.count)
+    }
+    
+    func loadMoreCells() {
+        numberOfCells = numberOfCells + 5
+        let query = PFQuery(className:"Posts")
+        query.includeKey("author")
+        query.order(byDescending: "createdAt")
+        query.limit = numberOfCells
+        
+        query.findObjectsInBackground { (posts, error) in
+            if posts != nil {
+                // Use new data to update data source
+                self.posts = posts!
+                // Update flag
+                self.isMoreDataLoading = false
+                // Stop the loading indicator
+                self.loadingMoreView?.stopAnimating()
+                self.tableView.reloadData()
+
+            } else {
+                print("error loading more cells")
+            }
+        }
+        print("loading more cells")
+        print(posts.count)
+    }
+    
+//    https://guides.codepath.org/ios/Table-View-Guide#adding-pull-to-refresh
+    func scrollViewDidScroll(_ scrollView: UIScrollView) {
+        if (!isMoreDataLoading) {
+            // Calculate the position of one screen length before the bottom of the results
+            let scrollViewContentHeight = tableView.contentSize.height
+            let scrollOffsetThreshold = scrollViewContentHeight - tableView.bounds.size.height
+            
+            // When the user has scrolled past the threshold, start requesting
+            if (scrollView.contentOffset.y > scrollOffsetThreshold && tableView.isDragging) {
+                isMoreDataLoading = true
+                
+                // Update position of loadingMoreView, and start loading indicator
+                let frame = CGRect(x: 0, y: tableView.contentSize.height, width: tableView.bounds.size.width, height: InfiniteScrollActivityView.defaultHeight)
+                loadingMoreView?.frame = frame
+                loadingMoreView!.startAnimating()
+                
+                loadMoreCells()
+            }
+        }
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return posts.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "PostCell", for: indexPath) as! PostCell
+        print("indexPath.row")
+        print(indexPath.row)
+        let post = posts[indexPath.row]
+        
+        let user = post["author"] as! PFUser
+        cell.usernameLabel.text = user.username
+        
+        cell.captionLabel.text = post["caption"] as? String
+        
+        let imageFile = post["image"] as! PFFileObject
+        let urlString = imageFile.url!
+        let url = URL(string: urlString)!
+        
+        cell.photoView.af.setImage(withURL: url)
+        return cell
+    }
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
+
+//https://guides.codepath.org/ios/Table-View-Guide#adding-pull-to-refresh
+class InfiniteScrollActivityView: UIView {
+    var activityIndicatorView: UIActivityIndicatorView = UIActivityIndicatorView()
+    static let defaultHeight:CGFloat = 60.0
+    
+    required init?(coder aDecoder: NSCoder) {
+        super.init(coder: aDecoder)
+        setupActivityIndicator()
+    }
+    
+    override init(frame aRect: CGRect) {
+        super.init(frame: aRect)
+        setupActivityIndicator()
+    }
+    
+    override func layoutSubviews() {
+        super.layoutSubviews()
+        activityIndicatorView.center = CGPoint(x: self.bounds.size.width/2, y: self.bounds.size.height/2)
+    }
+    
+    func setupActivityIndicator() {
+        activityIndicatorView.style = .medium
+        activityIndicatorView.hidesWhenStopped = true
+        self.addSubview(activityIndicatorView)
+    }
+    
+    func stopAnimating() {
+        self.activityIndicatorView.stopAnimating()
+        self.isHidden = true
+    }
+    
+    func startAnimating() {
+        self.isHidden = false
+        self.activityIndicatorView.startAnimating()
+    }
+}
